ecc8558ceb483f93a629194d6e023d2e6ecec0a6
Kiendz12 <127928090+Kiendz12@users.noreply.github.com>
2024-10-27 21:04:22 +0700

    Add files via upload

---
+++ xin_giay_xac_nhan/btl1.js
@@ -0,0 +1,36 @@
+function addRow() {
+    var loaiGiay = document.getElementById("loai-giay").value;
+    if (loaiGiay === "") {
+        alert("Vui lòng chọn loại giấy xác nhận!");
+        return;
+    }
+
+    var table = document.getElementById("confirmation-table").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow();
+    var sttCell = newRow.insertCell(0);
+    var loaiGiayCell = newRow.insertCell(1);
+    var ngayTaoCell = newRow.insertCell(2);
+    var trangThaiCell = newRow.insertCell(3);
+    var noiNhanCell = newRow.insertCell(4);
+    var ghiChuCell = newRow.insertCell(5);
+    var huyCell = newRow.insertCell(6);
+
+    var rowCount = table.rows.length;
+    sttCell.innerHTML = rowCount -1;
+
+    loaiGiayCell.innerHTML = loaiGiay;
+
+    var currentDate = new Date();
+    var dateString = currentDate.toLocaleDateString('vi-VN') + ' ' + currentDate.toLocaleTimeString('vi-VN', { hour12: false });
+    ngayTaoCell.innerHTML = dateString;
+
+    trangThaiCell.innerHTML = "Chờ xác nhận!";
+    noiNhanCell.innerHTML = "";
+    ghiChuCell.innerHTML = "";
+    huyCell.innerHTML = '<button onclick="deleteRow(this)">Hủy</button>';
+}
+
+function deleteRow(btn) {
+    var row = btn.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
