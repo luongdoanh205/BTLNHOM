455dcad3cac1a10335efb266ff6cbcf5c122de81
cuongdeptraivcb <domanhcuongcute@gmail.com>
2024-10-31 10:00:38 +0700

    Update xebuyt.js

--- xeBuyt/xebuyt.js
+++ xeBuyt/xebuyt.js
@@ -11,35 +11,26 @@ document.getElementById("imageInput").addEventListener("change", function(event)
     }
 });
 function showAdditional() {
-    // Hiển thị phần lựa chọn tiếp theo khi chọn Tùy chọn 1
+    
     document.getElementById("additional-options").classList.remove("hidden");
 }
 
 function hideAdditional() {
-    // Ẩn phần lựa chọn tiếp theo khi chọn Tùy chọn 2
+    
     document.getElementById("additional-options").classList.add("hidden");
 }
 function addRow() {
-    var loaiGiay = document.getElementById("loai-tuyen").value;
-    if (loaiGiay === "") {
-        alert("Vui lòng chọn loại giấy xác nhận!");
-        return;
-    }
-
     var table = document.getElementById("confirmation-table").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow();
-    var sttCell = newRow.insertCell(0);
-    var loaiGiayCell = newRow.insertCell(1);
-    var ngayTaoCell = newRow.insertCell(2);
-    var trangThaiCell = newRow.insertCell(3);
-    var noiNhanCell = newRow.insertCell(4);
-    var ghiChuCell = newRow.insertCell(5);
-    var huyCell = newRow.insertCell(6);
+    var sttCell = newRow.insertCell(0);  
+    var ngayTaoCell = newRow.insertCell(1);  
+    var trangThaiCell = newRow.insertCell(2);  
+    var noiNhanCell = newRow.insertCell(3);  
+    var ghiChuCell = newRow.insertCell(4); 
+    var huyCell = newRow.insertCell(5);  
 
     var rowCount = table.rows.length;
-    sttCell.innerHTML = rowCount -1;
-
-    loaiGiayCell.innerHTML = loaiGiay;
+    sttCell.innerHTML = rowCount - 1;
 
     var currentDate = new Date();
     var dateString = currentDate.toLocaleDateString('vi-VN') + ' ' + currentDate.toLocaleTimeString('vi-VN', { hour12: false });
